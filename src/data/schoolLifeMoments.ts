4832557e4a6a25c28aefea95b1155d3325920997
Azad Mohanty <mohantyazad89@gmail.com>
2025-11-08 16:02:33 +0530

    added eents

--- src/data/schoolLifeMoments.ts
+++ src/data/schoolLifeMoments.ts
@@ -1,8 +1,8 @@
 import type { Moment } from '../types';
 
 export const schoolLifeMomentsData: Moment[] = [
-  { src: '/images/pages/home/moments-1.jpg', caption: 'Annual Day Celebrations' },
-  { src: '/images/pages/home/moments-2.jpg', caption: 'Innovations at Science Fair' },
-  { src: '/images/pages/home/moments-3.jpg', caption: 'Victories at Sports Meet' },
+  { src: '/images/pages/home/annualday.webp', caption: 'Annual Day Celebrations' },
+  { src: '/images/pages/home/competitions.webp', caption: 'Various Competitions' },
+  { src: '/images/pages/home/morningassembly.webp', caption: 'Morning School Assembly' },
   { src: '/images/pages/home/moments-4.jpg', caption: 'Creative Art Exhibition' },
 ];
