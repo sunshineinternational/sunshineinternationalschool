67e08c2d03b9755f927af3b89af1f7df128b1d18
sunshineinternational <schoolsunshineinternational@gmail.com>
2025-12-01 17:57:38 +0530

    added sitemap and robot

--- src/data/teachers.ts
+++ src/data/teachers.ts
@@ -4,16 +4,16 @@ export const leadershipData: Teacher[] = [
   {
     name: "Praful Kumar Rath",
     role: "President",
-    qualification: "MA, M.Phil",
-    experience: "25",
+    qualification: "",
+    experience: "",
     img: "/images/staff/prafulkumarrath.jpg",
     testimonial: "At Sunshine International School, we believe in nurturing young minds to become global citizens. Our commitment to excellence in education, combined with a focus on character development, ensures that our students are well-prepared to face the challenges of tomorrow. We take pride in our state-of-the-art facilities, experienced faculty, and innovative curriculum that fosters creativity, critical thinking, and leadership skills in every student."
   },
   {
     name: "Sunil Ku Mohanty",
     role: "Principal",
-    qualification: "B.Sc, B.Ed",
-    experience: "18",
+    qualification: "",
+    experience: "",
     img: "/images/staff/sunilkumohanty.jpg",
     testimonial: "As the Principal of Sunshine International School, I am honored to lead a team of dedicated educators who are committed to providing a nurturing and stimulating learning environment. Our holistic approach to education ensures that each student receives personalized attention and opportunities to excel in academics, sports, and co-curricular activities. We strive to create a community where every child feels valued and inspired to achieve their full potential."
   }
@@ -31,7 +31,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Sumitra Sahu",
         role: "Hindi",
-        qualification: "B.A., D.El.Ed.",
+        qualification: "D.El.Ed.",
         experience: "12",
         img: "/images/staff/sumitrasahu.jpg",
         testimonial: "It is the place of learning, but a garden where dreams take root and grow"
@@ -39,7 +39,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Bhagyalaxmi Panda",
         role: "English",
-        qualification: "B.A., D.El.Ed.",
+        qualification: "D.El.Ed.",
         experience: "14",
         img: "/images/staff/bhagyalaxmipanda.jpg",
         testimonial: "The school offers a perfect balance between academics, decipline and creativity."
@@ -47,7 +47,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Deepti Rani Panda",
         role: "SST",
-        qualification: "B.A., D.El.Ed.",
+        qualification: "D.El.Ed.",
         experience: "19",
         img: "/images/staff/deeptiranipanda.jpg",
         testimonial: "Proud to be part of a Sunshine international school, that inspires learning and excellence everyday."
@@ -55,7 +55,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Sunita Kumari Gouda",
         role: "SST",
-        qualification: "B.A., B.Ed.",
+        qualification: "B.Ed.",
         experience: "10",
         img: "/images/staff/sunitakumarigouda.jpg",
         testimonial: "A school that inspires curiosity, encourages innovation and celebrate success."
@@ -63,7 +63,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Archana Nayak",
         role: "English & Math",
-        qualification: "B.Com., B.Ed.",
+        qualification: "B.Com,B.Ed.",
         experience: "15",
         img: "/images/staff/archananayak.jpg",
         testimonial: "Our school is where knowledge meets kindness and every student learns to shine"
@@ -71,7 +71,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Swagatika Panda",
         role: "Math & Odia",
-        qualification: "B.A., D.El.Ed.",
+        qualification: "D.El.Ed.",
         experience: "9",
         img: "/images/staff/swagatikapanda.jpg",
         testimonial: "Our school is where knowledge meets kindness and every student learns to shine."
@@ -79,7 +79,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Kusuma Behera",
         role: "Math",
-        qualification: "B.A., D.El.Ed.",
+        qualification: "D.El.Ed.",
         experience: "1",
         img: "/images/staff/kusumabehera.jpg",
         testimonial: "Here, education is not just about books- it's about building character and confidence."
@@ -87,7 +87,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Jogindra Behera",
         role: "Science",
-        qualification: "B.Sc., D.El.Ed.",
+        qualification: "D.El.Ed.",
         experience: "7",
         img: "/images/staff/jogindrabehera.jpg",
         testimonial: "Teachers have the unique opportunity to form strong bonds with students, becoming trusted mentors who provide support and encoragement"
@@ -95,7 +95,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Sangita Muni",
         role: "Odia",
-        qualification: "B.A., B.Ed.",
+        qualification: "B.Ed.",
         experience: "14",
         img: "/images/staff/sangitamuni.jpg",
         testimonial: "The school that curiosity encourages innovation and celebrate success"
@@ -103,7 +103,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Asharani Nayak",
         role: "Math",
-        qualification: "B.Sc., B.Ed.",
+        qualification: "B.Ed.",
         experience: "2",
         img: "/images/staff/asharaninayak.jpg",
         testimonial: "A truly nurturing environment where teachers care and students grow with confidence."
@@ -111,7 +111,7 @@ export const facultyData: Teacher[] = [
     {
         name: "B.Basanti Reddy",
         role: "Science",
-        qualification: "B.Sc., D.El.Ed.",
+        qualification: "D.El.Ed.",
         experience: "4",
         img: "/images/staff/bbasantireddy.jpg",
         testimonial: "An excellent place for holistic development - academically strong and morally rich"
@@ -127,7 +127,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Pritujita Mohanty",
         role: "Math & English",
-        qualification: "M.A., B.A.",
+        qualification: "M.A.",
         experience: "5",
         img: "/images/staff/pritujitamohanty.jpg",
         testimonial: "It is the place of learning, but a garden where dreams take root and grow"
@@ -135,7 +135,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Rina Samal",
         role: "English & Odia",
-        qualification: "B.A., B.Ed.",
+        qualification: "B.Ed.",
         experience: "5",
         img: "/images/staff/rinasamal.jpg",
         testimonial: "Best School in my life. Best opportunity by Sunshine International School. Thank You"
@@ -143,7 +143,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Anjali Sahu",
         role: "Social science",
-        qualification: "B.A., M.A., B.Ed.",
+        qualification: "M.A., B.Ed.",
         experience: "11",
         img: "/images/staff/anjalisahu.jpg",
         testimonial: "We get various type of knowledge from here i.e spiritual and educational also."
@@ -151,7 +151,7 @@ export const facultyData: Teacher[] = [
     {
         name: "Rasmita Gouda",
         role: "English",
-        qualification: "B.A., B.Ed.",
+        qualification: "B.Ed.",
         experience: "10",
         img: "/images/staff/rasmitagouda.jpg",
         testimonial: "The school offers a perfect balance between academic, disciplines and creativity"
