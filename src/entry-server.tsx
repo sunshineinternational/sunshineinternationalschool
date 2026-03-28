3428d46fbd46e6ed28310e2a99dfab2e3ef056f6
sunshineinternational <schoolsunshineinternational@gmail.com>
2026-03-28 07:14:59 +0530

    enntry server

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { MemoryRouter } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { routes } from './routes';
-import Layout from './components/layout/Layout';
 
-// This is a robust 'mini-router' for our snapshot tool
 export function render(url: string) {
-  // 1. Find the correct page component for this URL
+  // 1. Find the correct page component
   const path = url === '/' ? '/' : url;
   const childRoute = routes[0].children?.find(r => 
     r.index ? path === '/' : `/${r.path}` === path
@@ -14,15 +13,16 @@ export function render(url: string) {
 
   const PageComponent = childRoute?.element || <div>404</div>;
 
-  // 2. Render it inside the Layout and Theme
+  // 2. Wrap it in a 'MemoryRouter' so Links and Hooks work perfectly
   return renderToString(
-    <ThemeProvider>
-      <div className="font-['Roboto'] min-h-screen flex flex-col">
-          {/* We manually recreate the layout here to avoid browser-only code in server build */}
-          <main className="flex-grow" style={{ paddingTop: '75px' }}>
-              {PageComponent}
-          </main>
-      </div>
-    </ThemeProvider>
+    <MemoryRouter initialEntries={[url]}>
+      <ThemeProvider>
+        <div className="font-['Roboto'] min-h-screen flex flex-col">
+            <main className="flex-grow" style={{ paddingTop: '75px' }}>
+                {PageComponent}
+            </main>
+        </div>
+      </ThemeProvider>
+    </MemoryRouter>
   );
 }
