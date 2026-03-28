255114de5024fa7986ce86114a8449828d445944
sunshineinternational <schoolsunshineinternational@gmail.com>
2026-03-28 07:08:02 +0530

    entry server

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom/server';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { routes } from './routes';
-import { useRoutes } from 'react-router-dom';
+import Layout from './components/layout/Layout';
 
-const MainApp = () => {
-    const element = useRoutes(routes);
-    return (
-        <ThemeProvider>
-            {element}
-        </ThemeProvider>
-    );
-};
-
+// This is a robust 'mini-router' for our snapshot tool
 export function render(url: string) {
+  // 1. Find the correct page component for this URL
+  const path = url === '/' ? '/' : url;
+  const childRoute = routes[0].children?.find(r => 
+    r.index ? path === '/' : `/${r.path}` === path
+  );
+
+  const PageComponent = childRoute?.element || <div>404</div>;
+
+  // 2. Render it inside the Layout and Theme
   return renderToString(
-    <StaticRouter location={url}>
-      <MainApp />
-    </StaticRouter>
+    <ThemeProvider>
+      <div className="font-['Roboto'] min-h-screen flex flex-col">
+          {/* We manually recreate the layout here to avoid browser-only code in server build */}
+          <main className="flex-grow" style={{ paddingTop: '75px' }}>
+              {PageComponent}
+          </main>
+      </div>
+    </ThemeProvider>
   );
 }
